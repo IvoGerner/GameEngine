f107494a747ecc2e50a8023113c7da26d1fb18dd
Ivo <i.gerner@outlook.com>
2020-05-05 02:18:34 +0200

    init

---
+++ GameEngine-core/src/graphics/window.cpp
@@ -0,0 +1,19 @@
+#include "window.h"
+
+namespace engine {
+	namespace graphics {
+		Window::Window(const char* name, int width, int height) {
+			m_Name = name;
+			m_Width = width;
+			m_Height = height;
+		}
+
+		Window::~Window() {
+
+		}
+
+		void Window::update() const {
+
+		}
+	}
+}
